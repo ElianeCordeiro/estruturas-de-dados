303ebe6fa1a7252756ecb7508c21fcfa6b86b3fa
Eliane Cordeiro <elianemcordeiro1@gmail.com>
2023-11-12 15:55:07 -0300

    atualizacao avl recostrucao assinatura dos metodos

--- arvores/avlrecuperacao/No.java
+++ arvores/avlrecuperacao/No.java
@@ -2,4 +2,96 @@ package avlrecuperacao;
 
 public class No {
 
+
+
+	int info;
+
+	No esquerda;
+
+	No direita;
+
+	int altura;
+
+
+
+	public No(int info) {
+
+	    this.info = info;
+
+	    this.esquerda = null;
+
+	    this.direita = null;
+
+	    this.altura = 1;
+
+	}
+
+
+
+	public int getInfo() {
+
+		return info;
+
+	}
+
+
+
+	public void setInfo(int info) {
+
+		this.info = info;
+
+	}
+
+
+
+	public No getEsquerda() {
+
+		return esquerda;
+
+	}
+
+
+
+	public void setEsquerda(No esquerda) {
+
+		this.esquerda = esquerda;
+
+	}
+
+
+
+	public No getDireita() {
+
+		return direita;
+
+	}
+
+
+
+	public void setDireita(No direita) {
+
+		this.direita = direita;
+
+	}
+
+
+
+	public int getAltura() {
+
+		return altura;
+
+	}
+
+
+
+	public void setAltura(int altura) {
+
+		this.altura = altura;
+
+	}
+
+
+
 }
+
+
